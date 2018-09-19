54367f4465857d220045ad568d7abd4383761a98
tampieri <higortampieri@gmail.com>
2018-09-19 10:47:44 -0300

    Navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+import { NavController, IonicPage } from 'ionic-angular';
+
 
 @IonicPage()
 @Component({
@@ -11,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
