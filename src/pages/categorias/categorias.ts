db97c57b7ba497d403a313d970465734c4441802
tampieri <higortampieri@gmail.com>
2018-09-19 14:50:46 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -35,8 +35,6 @@ export class CategoriasPage {
     .subscribe(response =>{
       this.items = response;
     },
-    error => {
-      console.log(error);
-    });   
+    error => {});   
   }
 }
