1455ca196d5a65ed8132c11144b35bd765c7465f
tampieri <higortampieri@gmail.com>
2018-09-20 08:59:27 -0300

    Botao de logout no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
+import { AuthService } from './../services/auth.service';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
- @Component({
+@Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage: string = 'HomePage';
    pages: Array<{title: string, component: string}>;
-   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+   constructor(
+      public platform: Platform, 
+      public statusBar: StatusBar, 
+      public splashScreen: SplashScreen,
+      public auth: AuthService
+    ) {
     this.initializeApp();
      // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Categorias', component: 'CategoriasPage' },
-      { title: 'Profile', component: 'ProfilePage' }
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Logout', component: '' }
     ];
    }
-   initializeApp() {
+  initializeApp() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -26,9 +33,16 @@ export class MyApp {
       this.splashScreen.hide();
     });
   }
-   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  openPage(page: {title: string, component: string}) {
+
+    switch(page.title){
+      case 'Logout':
+      this.auth.logout();
+      this.nav.setRoot('HomePage');
+      break;
+
+      default:
+      this.nav.setRoot(page.component);
+    }   
   }
 }
