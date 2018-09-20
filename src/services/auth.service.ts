30ba67e5bdc74b588bf0bb194d8172a894c7f344
tampieri <higortampieri@gmail.com>
2018-09-20 08:47:07 -0300

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -13,14 +13,26 @@ export class AuthService {
 
      constructor(public http: HttpClient, public storage: StorageService) {
     }
-     authenticate(creds : CredenciaisDTO) {
+    authenticate(creds : CredenciaisDTO) {
         return this.http.post(
             `${API_CONFIG.baseUrl}/login`, 
             creds,
             {
                 observe: 'response',
                 responseType: 'text'
-            });
+            }
+        );
+    }
+
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`, 
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
     }
 
     successfulLogin(authorizationValue : string) {
