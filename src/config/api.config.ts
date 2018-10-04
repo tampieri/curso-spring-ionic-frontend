cf0af3aa88bc4d8da2486159560e98ece4f30ece
tampieri <higortampieri@gmail.com>
2018-10-04 08:37:28 -0300

    Alterações pontuais

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:8090",
-    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-higor"
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-higor1"
 }
